deb473296bd975d3740817250ae33620307b3045
Valentine <razorsmile@users.noreply.github.com>
2025-11-05 11:15:42 +0000

    Add signup redirect to identity handler
    - Automatically redirect to /admin after completing invitation signup
    - Previously only redirected on login, not on initial password setup

--- public/identity-redirect.js
+++ public/identity-redirect.js
@@ -4,7 +4,13 @@
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
     if (!user) {
+      // Redirect to admin after login
       window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+
+      // Redirect to admin after signup/invitation completion
+      window.netlifyIdentity.on("signup", () => {
         document.location.href = "/admin/";
       });
     }
